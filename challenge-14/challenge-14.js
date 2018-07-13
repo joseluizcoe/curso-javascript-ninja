437d132e38f6606a3c84de695ce7396ec5395206
José Luiz Coe <jose.souza@catho.com>
2018-07-13 10:23:50 -0300

    Utilizando for

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -10,18 +10,11 @@
   Os números devem ser de 1 a 10.
   Mostre esse array no console.
   */
-  var numberObjects = [
-    { number: 1 },
-    { number: 2 },
-    { number: 3 },
-    { number: 4 },
-    { number: 5 },
-    { number: 6 },
-    { number: 7 },
-    { number: 8 },
-    { number: 9 },
-    { number: 10 },
-  ];
+  var numberObjects = []
+  for (var i=1; i <=10; i++) {
+    numberObjects.push({ number: i })
+  };
+
   console.log( 'Number Objects Array:' );
   console.log( numberObjects );
 
